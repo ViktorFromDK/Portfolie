0a4019d472686d8b837c62afa921d289fc88cd6c
Viktor Espenhain <viktor@espenhain.com>
2023-10-13 12:42:53 +0200

    Add files via upload

--- projects/spillegogsamarbejde/sketch.js
+++ projects/spillegogsamarbejde/sketch.js
@@ -14,15 +14,15 @@ let SideHit // Makes the ball hitting the border sound global
 let TopHit // Makes the ball hitting the top sound global
 
 function preload(){
-  background = createImg('projects/spillegogsamarbejde/Resources/background.png',''); // Defines the picture for the background
+  background = createImg('Resources/Background.png',''); // Defines the picture for the background
 	background.hide(); // Hides the background picture
-  ball = createImg('projects/spillegogsamarbejde/Resources/ball.png',''); // Defines the picture for the ball
+  ball = createImg('Resources/Ball.png',''); // Defines the picture for the ball
   ball.hide(); // Hides the ball picture
-  PlayerHit = createAudio('projects/spillegogsamarbejde/Resources/HitPlayer.mp3',''); // Defines the audio for Player Hit
+  PlayerHit = createAudio('Resources/HitPlayer.mp3',''); // Defines the audio for Player Hit
   PlayerHit.hide(); // Hides the player hit sound
-  SideHit = createAudio('projects/spillegogsamarbejde/Resources/HitSide.mp3',''); // Defines the audio for Side Hit
+  SideHit = createAudio('Resources/HitSide.mp3',''); // Defines the audio for Side Hit
   SideHit.hide(); // Hides the side hit sound
-  TopHit = createAudio('projects/spillegogsamarbejde/Resources/HitTop.mp3','') // Defines the audio for Top Hit
+  TopHit = createAudio('Resources/HitTop.mp3','') // Defines the audio for Top Hit
   TopHit.hide(); // Hides the top hit sound
 }
 
