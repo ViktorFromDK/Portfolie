cece2abf594a82a040c0294cfcf44206799f6619
Viktor Espenhain <viktor@espenhain.com>
2024-05-08 14:48:49 +0200

    Update script.js

--- projects/Databaser/script.js
+++ projects/Databaser/script.js
@@ -5,7 +5,6 @@ let data;
 var theme = null // Sætter variablen til ingenting
 var explanation = "Korrekt svar:" // Sætter variablen til ingenting
 var image = null // Sætter variablen til ingenting
-let show = 0;
 
 function next() {
   var pic;
@@ -37,13 +36,7 @@ function reveal(){
 }
 
 function hasLearned(){
-  if (show == 0) {
-    document.getElementById('choose').innerHTML = listWithLearned; // Returnere information til HTML og "variablen" "valg"
-    show = 1
-  } else {
-    document.getElementById('choose').innerHTML = ""; // Returnere information til HTML og "variablen" "valg"
-    show = 0
-  }
+  document.getElementById('choose').innerHTML = listWithLearned; // Returnere information til HTML og "variablen" "valg"
 }
 
 function findText(x){
